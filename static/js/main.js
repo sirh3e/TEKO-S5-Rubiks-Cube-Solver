f635f0717fb85ae08c8da6c64eb29e75724660bc
Patrik Burkhalter <20504120+pburkhalter@users.noreply.github.com>
2024-01-21 16:03:46 +0100

    Update static/js/main.js
    Co-authored-by: bariod <35639254+bariod@users.noreply.github.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {initCube, animateCube, onMouseClick} from './controls.js';
+import { initCube, animateCube, onMouseClick } from './controls.js';
 
 
 const config = {
